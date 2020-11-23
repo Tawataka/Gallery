61a52c58080b50efc17e605b4dac03225c2d5408
Tawataka <treynavi@ucsc.edu>
2020-11-23 14:24:24 -0800

    added node.js

--- app.js
+++ app.js
@@ -1,26 +1,7 @@
-const http = require("http")
-const fs = require("fs")
+const express = require("express")
+const app = express()
 
-const port = 3000
+app.set("view engine", "ejs")
+app.use(express.static("public"))
 
-const server = http.createServer(function(req, res){
-    res.writeHead(200, {"content-type": "text/html"})
-    fs.readFile("public/index.html", function(error, data){
-        if(error){
-            res.writeHead(404)
-            res.write("file not found")
-        }
-        else{
-            res.write(data)
-        }
-        res.end()
-    })
-})
-server.listen(port, function(error){
-    if(error){
-        console.log("something went wrong", error)
-    }
-    else{
-        console.log("server is listening " + port)
-    }
-})
+app.listen(3000)
