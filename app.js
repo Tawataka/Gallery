1fc29e4ae6df4efd14ca8c8f280c2395ba4c2386
Tawataka <treynavi@ucsc.edu>
2020-11-23 14:08:22 -0800

    added node.js

---
+++ app.js
@@ -0,0 +1,26 @@
+const http = require("http")
+const fs = require("fs")
+
+const port = 3000
+
+const server = http.createServer(function(req, res){
+    res.writeHead(200, {"content-type": "text/html"})
+    fs.readFile("public/index.html", function(error, data){
+        if(error){
+            res.writeHead(404)
+            res.write("file not found")
+        }
+        else{
+            res.write(data)
+        }
+        res.end()
+    })
+})
+server.listen(port, function(error){
+    if(error){
+        console.log("something went wrong", error)
+    }
+    else{
+        console.log("server is listening " + port)
+    }
+})
