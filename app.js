231c4c6cfb7192e12c4b13e9c3c81f0cb0998b16
Tawataka <treynavi@ucsc.edu>
2021-04-08 13:20:27 -0700

    added package app

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const app = express()
 const fs = require("fs")
 const stripe = require("stripe")(stripeSecretKey)
 
+const nodemailer = require('nodemailer');
+
 app.set("view engine", "ejs")
 app.use(express.json())
 app.use(express.static("public"))
@@ -31,10 +33,21 @@ app.get("", function(req, res){
 app.get("/about", function(req, res){
     
     res.render("about.ejs")
+    
 })
 app.get("/cart", function(req, res){
-    
-    res.render("cart.ejs")
+    fs.readFile("item.json", function(error, data){
+       
+        if(error){
+            res.status(500).end()
+        }
+        else{
+            res.render("cart.ejs", {
+                stripePublicKey: stripePublicKey,
+                items: JSON.parse(data)
+            })
+        }
+    })
 })
 
 app.post("/purchase", function(req, res){
@@ -47,12 +60,87 @@ app.post("/purchase", function(req, res){
             const itemsJson = JSON.parse(data)
             const itemsArray = itemsJson.merch.concat(itemsJson.merch)
             let total = 0
+            let statement = `
+            <p style="font-family: Arial; font-size: 2.0em;">Order Received</p>
+            <div style="width: 80%;">
+                <table style="font-family: Arial, Helvetica, sans-serif; border-collapse: collapse; width: 100%;">
+                    <tr style="border-bottom: 1px solid black; margin-right: 4.5em; padding-bottom: 10px; margin-top: 10px;">
+                        <th></th>
+                        <th>ITEM</th>
+                        <th>PRICE</th>
+                        <th>QUANTITY</th>
+                    </tr> 
+            `
             req.body.items.forEach(function(item){
                 const itemJson = itemsArray.find(function(i){
                     return i.id == item.id
                 })
                 total = total + itemJson.price * item.quantity
+                statement = statement + `
+                    <tr>
+                        <td> <img src="images/` + itemJson.image + `" width="100" height="100"></td>
+                        <td style="padding: 40px;">` + itemJson.name + `</td>
+                        <td style="padding: 40px;">$` + itemJson.price/100 + `</td>
+                        <td style="padding: 40px;"> X` + item.quantity +`</td>
+                    </tr>
+                `
             })
+            statement = statement + `
+                </table>
+                <div style="text-align: right;">
+                    <strong class="cart-total-title">Total</strong>
+                    <span class="cart-total-price">$` + total/100 + `</span>
+                </div>
+            </div>
+            <div style="width: 40%; background-color: #f7dd4e; padding: 20px;">
+				<p style="font-family: Arial; font-size: 1.5em; padding: 0px;">Ship To</p>
+				<div style="border-bottom: 1px solid black; padding: 0px;"></div>
+				<table style="font-family: Arial, Helvetica, sans-serif; border-collapse: collapse; width: 100%;">
+					
+					<tr>
+						<td>` + req.body.name + `</td>
+					</tr>
+					<tr>
+						<td>` + req.body.address + `</td>
+					</tr>
+					<tr>
+						<td>` + req.body.city + `, ` + req.body.state + `, ` + req.body.zip + `</td>
+					</tr>
+					
+				</table>
+			</div>
+
+            `
+
+            var maillist = [
+                req.body.email,
+                'tawa.tv.777@gmail.com',
+            ];
+             
+
+            var transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: 'tawa.tv.777@gmail.com',
+                pass: 'Tawataka3737'
+            }
+            });
+        
+            var mailOptions = {
+                from: 'tawa.tv.777@gmail.com',
+                to: maillist, 
+                subject: 'Art Gallery Purchase Complete',
+                html: statement 
+            };
+        
+            transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Email sent: ' + info.response);
+            }
+            });
+
             stripe.charges.create({
                 amount: total,
                 source: req.body.stripeTokenId,
